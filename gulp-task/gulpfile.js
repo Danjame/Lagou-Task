a04be90239dd1c6f66bd7fd92c7cc42241f68d27
danjame <danjamexie@gmail.com>
2020-06-16 18:48:37 +0800

    gulp-toolkit completed

--- gulp-task/gulpfile.js
+++ gulp-task/gulpfile.js
@@ -1,41 +1,46 @@
 // 实现这个项目的构建任务
 
-const { src, dest, parallel, series } = require('gulp');
+const { src, dest, parallel, series, watch } = require('gulp');
 const swig = require('gulp-swig');
 const sass = require('gulp-sass');
 const babel = require('gulp-babel');
 const imagemin = require('gulp-imagemin');
 
 const del = require('del');
+const browserSync = require('browser-sync');
+const bs = browserSync.create();
 
 //编译文件
 const page = () => {
     return src('src/**/*.html', { base: 'src' })
-        .pipe(swig())
+        .pipe(swig({defaults: {cache:false}}))
         .pipe(dest('realse'))
+        .pipe(bs.reload({stream: true}))
 }
 
 const style = () => {
     return src('src/**/*.scss', { base: 'src' })
         .pipe(sass({ outputStyle: 'expanded' }))
         .pipe(dest('realse'))
+        .pipe(bs.reload({stream: true}))
 }
 
 const script = () => {
     return src('src/**/*.js', { base: 'src' })
         .pipe(babel({ presets: ['@babel/preset-env'] }))
         .pipe(dest('realse'))
+        .pipe(bs.reload({stream: true}))
 }
 
 //图片压缩
 const image = () => {
-    return src('src/**/images/**', { base: 'src' })
+    return src('src/assets/images/**', { base: 'src' })
         .pipe(imagemin())
         .pipe(dest('realse'))
 }
 //字体压缩
 const font = () => {
-    return src('src/**/fonts/**', { base: 'src' })
+    return src('src/assets/fonts/**', { base: 'src' })
         .pipe(imagemin())
         .pipe(dest('realse'))
 }
@@ -45,19 +50,42 @@ const extra = () => {
     return src('public/**', { base: 'public' })
         .pipe(dest('realse'))
 }
-
-const clean = ()=>{
+//清除realse文件夹
+const clean = () => {
     return del(['realse'])
 }
 
-const compile = parallel(page, style, script);
+//开启服务器并监视文件变化
+const server = () => {
+    watch('src/**/*.html', page);
+    watch('src/**/*.scss', style);
+    watch('src/**/*.js', script);
 
-// const build = series(clean, parallel(compile, extra));
+    watch([
+        'src/assets/images/**',
+        'src/assets/fonts/**',
+        'public/**'
+    ], bs.reload);
 
+    bs.init(
+        {
+            server: {
+                baseDir: 'realse',
+                files: 'realse/**',
+                routes: {
+                    '/node_modules': 'node_modules'
+                }
+            }
+        }
+    )
+}
 
+const compile = parallel(page, style, script, font, image);
+
+const build = series(clean, parallel(compile, extra));
 
 module.exports = {
     compile,
-    image,
-    font
+    build,
+    server,
 }
